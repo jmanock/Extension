8cd1a635bd1d6246e24195c5cb92de467e2fb023
jmanock <jonmanock@gmail.com>
2015-10-13 07:57:31 -0400

    one more tuts

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-  /* Todo
-    ~ Scrape nhl site somehow
-    ~ Add games to html file
-  */
-  var url = 'https://redditclone.firebaseio.com';
-  var ref = new Firebase(url);
-  console.log(ref);
-});
+window.onload=function(){
+  document.getElementById('button').onclick = function(){
+    chrome.extension.sendMessage({
+      type:'color-divs'
+    });
+  };
+};
