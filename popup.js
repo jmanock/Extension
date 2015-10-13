814611a0b6a793c4c59c81de48f83b2667a5e626
jmanock <jonmanock@gmail.com>
2015-10-13 06:37:22 -0400

    trying out another tuts

--- popup.js
+++ popup.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
     ~ Scrape nhl site somehow
     ~ Add games to html file
   */
-  
+  var url = 'https://redditclone.firebaseio.com';
+  var ref = new Firebase(url);
+  console.log(ref);
 });
