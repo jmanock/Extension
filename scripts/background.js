b0d43e7ed1726915257a619f8bd9a0ba2261cf77
jmanock <jonmanock@gmail.com>
2015-10-16 09:40:06 -0400

    search box works, now to make it a little better

--- scripts/background.js
+++ scripts/background.js
@@ -91,4 +91,19 @@
     var something = new GolfReader();
     something.init();
   });
+  $(document).ready(function(){
+    $('#filter').keyup(function(){
+      var filter = $(this).val(),count = 0;
+      $('ul li').each(function(){
+        if($(this).text().search(new RegExp(filter, 'i'))<0){
+          $(this).fadeOut();
+        }else{
+          $(this).show();
+          count++;
+        }
+      });
+      var numberItems = count;
+      $('#filter-count').text('number of players'+count);
+    });
+  });
 })();
