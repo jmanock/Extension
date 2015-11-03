78e4542342632d37d07f03968597f8cb470eb4a2
jmanock <jonmanock@gmail.com>
2015-11-03 06:44:54 -0500

    updated the tournament

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,6 @@
 (function(){
   var GolfReader = function(){
-    var url = 'http://www.pgatour.com/data/r/494/leaderboard-v2.json';
+    var url = 'http://www.pgatour.com/data/r/489/leaderboard-v2.json';
     var scoreHolder = $('.leaderboard');
     var open = '<tr>';
     var close = '<tr>';
